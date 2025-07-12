a4b57e6897c3bd55d10e6ee26b7fbfbaea866791
yush2106 <gavinyu42@hotmail.com>
2025-07-12 22:02:39 +0800

    flask example

---
+++ flask/flask-main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)  #Flask app
+CORS(app)  #add CORS
+#app.config['DEBUG'] = True  #debug mode
+
+ProjectName = ""
+#ProjectName = "/MyWebAPI"
+
+@app.route(ProjectName + "/", methods=['GET'])
+def Hello():
+  return '<h1>Hello, My Flask</h1>'  #hello response
+
+@app.route(ProjectName + "/ResponseData", methods=['POST'])
+def ResponseData():
+  try:
+    requestData = request.get_json()  #get request data from client
+    receivedData = requestData["SendData"]  #get JSON data
+    responseMsg = {"ReturnMsg": receivedData}  #compose response message
+    return responseMsg  #return response message
+  except Exception as err:
+    responseMsg = {"ReturnMsg": str(err)}  #exception message
+    return responseMsg  #return response message
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000)
+  #serve(app, host="0.0.0.0", port=5000, threads=5)  #run waitress server
